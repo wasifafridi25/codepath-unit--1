0b69f8c0f554f50d411d8d6771d11134f197b978
Bhuiyan Shadman Wasif <waseefafridi39@gmail.com>
2024-02-24 10:58:33 -0800

    Initial commit

---
+++ post/src/App.jsx
@@ -0,0 +1,15 @@
+
+import "./App.css";
+import Post from "./components/Post";
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to Web102</h1>
+      <h2>Tell me what you are most excited about?</h2>
+      <Post />
+    </>
+  );
+}
+
+export default App;
---
+++ post/src/components/Post.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Post() {
+  return (
+    <section style={{ border: "3px solid red" }}>
+      <h3>Bhuiyan Shadman Wasif</h3>
+      <h4>I want to learn the power of students</h4>
+      <p>Rating: 0</p>
+    </section>
+  );
+}
